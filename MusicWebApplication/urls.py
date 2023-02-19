b1b53b4bb04ea90389091b9ede34f88c13d578f5
Remco <remcoritfeld@live.nl>
2023-02-19 18:37:48 +0100

    - Implemented the start of modules of the website

--- MusicWebApplication/urls.py
+++ MusicWebApplication/urls.py
@@ -13,9 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import django.urls
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # Selling midis & sheet music
+    path('webshop/', include('Webshop.urls')),
+    # # Transcribing audio to midi
+    # path('midiscribe/', include('')),
+    # # Online Synthesia
+    # path('pianoplayer', include('')),
 ]
